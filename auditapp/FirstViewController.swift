2afc93b45c5df25e8a8239c4eea81e07e82ba4b8
Steven Liu <SteveMBP@192-168-1-11.tpgi.com.au>
2018-01-31 16:47:31 +1100

    Initial Commit

---
+++ auditapp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  auditapp
+//
+//  Created by Steven Liu on 31/1/18.
+//  Copyright © 2018 Steven Liu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
